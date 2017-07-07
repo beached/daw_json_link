616a3a956bbfa0317d09540af9ad2df87c4f0925
Darrell Wright <dwright-git@dawdevel.ca>
2017-07-07 17:44:00 -0400

    .

--- include/daw_json_link.h
+++ include/daw_json_link.h
@@ -674,6 +674,9 @@ namespace daw {
 	    []( auto const &obj ) -> std::decay_t<decltype( member_name )> const & { return obj.member_name; } );
 
 #define link_json_streamable( json_name, member_name )                                                                 \
+	static_assert( std::is_convertible<decltype( operator<<( std::declval<std::ostream>( ), member_name ) ),           \
+	                                   std::ostream>::value,                                                           \
+	               #member_name" must have an operator<< overload" );                                                 \
 	link_json_string_fn(                                                                                               \
 	    json_name,                                                                                                     \
 	    []( auto &obj, daw::string_view value ) -> void {                                                              \
@@ -683,7 +686,7 @@ namespace daw {
 	    []( auto const &obj ) -> std::string { return boost::lexical_cast<std::string>( obj.member_name ); } );
 
 #define link_json_streamable_optional( json_name, member_name, default_value )                                         \
-	static_assert( std::is_convertible<decltype( operator<<( std::declval<std::ostream>( ), member_name ) ),           \
+	static_assert( std::is_convertible<decltype( operator<<( std::declval<std::ostream>( ), *member_name ) ),           \
 	                                   std::ostream>::value,                                                           \
 	               #member_name" must have an operator<< overload" );                                                 \
 	link_json_string_optional_fn( json_name,                                                                           \
