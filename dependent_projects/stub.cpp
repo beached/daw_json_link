7bf0cc09c06cc5f85f8377a760ebb7e53de79123
Darrell Wright <dwright-git@dawdevel.ca>
2018-08-03 22:02:40 -0400

    typo in cmake add_definitions instead of add_dependencie

--- dependent_projects/stub.cpp
+++ dependent_projects/stub.cpp
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2017 Darrell Wright
+// Copyright (c) 2018 Darrell Wright
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files( the "Software" ), to deal
@@ -20,17 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <cstdint>
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <vector>
-
 int main( int, char** ) {
-
-
-
-	return EXIT_SUCCESS;
+	return 0;
 }
 
 
