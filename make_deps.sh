e077f1e7cc5827cb259d31561a20582d6ffbd2d9
Darrell Wright <dwright-git@dawdevel.ca>
2018-11-17 20:34:07 -0500

    updated to use new make_dep

--- make_deps.sh
+++ make_deps.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#version 2018_11_17 
 
 [[ -d ./dependent_projects/build ]] || mkdir ./dependent_projects/build
+[[ -d ./dependent_projects/build/release ]] || mkdir ./dependent_projects/build/release
+[[ -d ./dependent_projects/build/debug ]] || mkdir ./dependent_projects/build/debug
 
-cd ./dependent_projects/build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cd ./dependent_projects/build/release
+cmake ../.. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+make clean
+make -j2
+cd ../debug
+cmake ../.. -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make clean
 make -j2
