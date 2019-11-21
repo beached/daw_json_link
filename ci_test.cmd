e374486f6da56b5c4bf02d1a643ec798001cada6
Darrell Wright <dwright-git@dawdevel.ca>
2019-11-21 18:22:14 -0500

    .

--- ci_test.cmd
+++ ci_test.cmd
@@ -1,8 +1,11 @@
-
-rename glean.cmake.renamewin glean.cmake
+copy glean.cmake.renamewin glean.cmake
 md build
 cd build
 pwd
+echo "Configuring project"
 cmake -DCMAKE_BUILD_TYPE=Debug ..
+echo "Starting dep build"
+cmake --build . --config Debug --target -j 2
+echo "Starting full build"
 cmake --build . --config Debug --target full -j 2
 ctest -C Debug
