478f9a7ccd08ee49943207d96166046c8e23a461
Darrell Wright <dwright-git@dawdevel.ca>
2019-11-25 21:51:25 -0500

    moved ci scripts to folder and removed ninja test

--- ci_scripts/ci_ubuntu_build.sh
+++ ci_scripts/ci_ubuntu_build.sh
@@ -3,6 +3,6 @@
 cp glean.cmake.renamewin glean.cmake
 mkdir build
 cd build
-CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --config Debug --target full -j 2
 
