49a39637cc7dafe17218ace17b4a1ce099762613
Darrell Wright <dwright-git@dawdevel.ca>
2019-11-25 21:47:04 -0500

    trying ninja on ubuntu build

--- ci_ubuntu_build.sh
+++ ci_ubuntu_build.sh
@@ -3,6 +3,6 @@
 cp glean.cmake.renamewin glean.cmake
 mkdir build
 cd build
-CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug ..
+CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
 cmake --build . --config Debug --target full -j 2
 
--- cookbook/automated_code_generation.md
+++ cookbook/automated_code_generation.md
@@ -1,2 +1,12 @@
 # Automated Code Generation
 
+What we need to begin
+```bash
+git clone https://github.com/beached/json_to_cpp 
+cd json_to_cpp
+mkdir build
+cd build
+cmake ..
+cmake --build . 
+```
+
