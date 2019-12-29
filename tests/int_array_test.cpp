6f3eaceee02386819bb1ec728a3ff161f8405323
Darrell Wright <dwright-git@dawdevel.ca>
2019-12-29 17:47:27 -0500

    bigger buffer

--- tests/int_array_test.cpp
+++ tests/int_array_test.cpp
@@ -74,7 +74,7 @@ static std::string_view make_int_array_data( ) {
 		}
 		result.back( ) = ']';
 		// Allow SSE Modes to have enough room
-		result += "                                                     ";
+		result += "                                                                                                                                                                                ";
 		result.shrink_to_fit( );
 		return result;
 	}( );
