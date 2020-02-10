b80fe450eab6ff658df8cc557b696e3698544b46
Darrell Wright <dwright-git@dawdevel.ca>
2020-02-10 08:53:33 -0500

    Added sanitizers in CI runs

--- ci_scripts/ci_macos_deps.sh
+++ ci_scripts/ci_macos_deps.sh
@@ -5,7 +5,7 @@ mkdir build
 cd build
 
 echo "Configuring project"
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DDAW_JSON_USE_SANITIZERS=On ..
 
 echo "Starting dep build"
 cmake --build . --config Debug --target -j 2
--- ci_scripts/ci_ubuntu_deps.sh
+++ ci_scripts/ci_ubuntu_deps.sh
@@ -5,7 +5,7 @@ mkdir build
 cd build
 
 echo "Configuring project"
-CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug ..
+CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug  -DDAW_JSON_USE_SANITIZERS=On ..
 
 echo "Starting dep build"
 cmake --build . --config Debug --target -j 2
