d30de03ff85ad77636dee288c18238054640881f
Darrell Wright <dwright-git@dawdevel.ca>
2020-02-10 12:39:56 -0500

    Removed sanitizer check, exposing constexpr bugs in some compilers

--- ci_scripts/ci_macos_deps.sh
+++ ci_scripts/ci_macos_deps.sh
@@ -5,7 +5,7 @@ mkdir build
 cd build
 
 echo "Configuring project"
-cmake -DCMAKE_BUILD_TYPE=Debug -DDAW_JSON_USE_SANITIZERS=On ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 
 echo "Starting dep build"
 cmake --build . --config Debug --target -j 2
--- ci_scripts/ci_ubuntu_deps.sh
+++ ci_scripts/ci_ubuntu_deps.sh
@@ -5,7 +5,7 @@ mkdir build
 cd build
 
 echo "Configuring project"
-CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug  -DDAW_JSON_USE_SANITIZERS=On ..
+CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug ..
 
 echo "Starting dep build"
 cmake --build . --config Debug --target -j 2
