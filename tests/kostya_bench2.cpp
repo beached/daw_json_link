8921d25cbab1c4014e79361a460f705d5ef673f1
Darrell Wright <beached@users.noreply.github.com>
2020-05-16 15:10:07 -0400

    minor update

--- tests/kostya_bench2.cpp
+++ tests/kostya_bench2.cpp
@@ -6,7 +6,6 @@
 // Official repository: https://github.com/beached/daw_json_link
 //
 
-#include <daw/json/daw_json_iterator.h>
 #include <daw/json/daw_json_link.h>
 #include <daw/json/daw_json_value_state.h>
 
@@ -15,14 +14,6 @@
 #include <sstream>
 #include <string_view>
 #include <unistd.h>
-
-struct coordinate_t {
-	double x;
-	double y;
-	double z;
-	// ignore string name
-	// ignore object opts
-};
 
 std::string read_file( std::string const &filename ) {
 	std::ifstream f( filename );
@@ -34,27 +25,23 @@ std::string read_file( std::string const &filename ) {
 }
 
 int main( int argc, char *argv[] ) {
-	std::string const text = read_file( "/tmp/1.json" );
+	std::string const json_text = read_file( "/tmp/1.json" );
 
-	auto const json_sv = std::string_view( text.data( ), text.size( ) );
-	double x = 0, y = 0, z = 0;
+	double x = 0;
+	double y = 0;
+	double z = 0;
 	int len = 0;
 
 	using namespace daw::json;
 	auto rng =
-	  from_json_unchecked<json_delayed<no_name>>( json_sv, "coordinates" );
+	  from_json_unchecked<json_delayed<no_name>>( json_text, "coordinates" );
 
 	auto val = json_value_state( );
 	for( json_pair item : rng ) {
 		val.reset( item.value );
-		x += from_json_unchecked<double>( val[0] );
-		y += from_json_unchecked<double>( val[1] );
-		z += from_json_unchecked<double>( val[2] );
-		/*
 		x += from_json_unchecked<double>( val["x"] );
 		y += from_json_unchecked<double>( val["y"] );
 		z += from_json_unchecked<double>( val["z"] );
-		 */
 		++len;
 	}
 	std::cout << x / len << '\n';
