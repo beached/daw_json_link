6a1016a0dc71377441d4ceb7c7e4bd2b89cd21e9
Darrell Wright <beached@users.noreply.github.com>
2020-05-16 15:10:43 -0400

    Added basic test2 for twitter:x

---
+++ tests/twitter_test_basic2.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/daw_json_link
+//
+
+#include "twitter_test2.h"
+
+#include <daw/daw_benchmark.h>
+#include <daw/daw_memory_mapped_file.h>
+#include <daw/daw_string_view.h>
+#include <daw/json/daw_json_link.h>
+
+#include <fstream>
+#include <iostream>
+#include <streambuf>
+
+int main( int argc, char **argv ) try {
+	if( argc < 2 ) {
+		std::cerr << "Must supply a file name\n";
+		exit( 1 );
+	}
+	using namespace daw::json;
+	auto const json_data1 = daw::filesystem::memory_mapped_file_t<>( argv[1] );
+	auto const json_sv1 =
+	  std::string_view( json_data1.data( ), json_data1.size( ) );
+
+	auto const twitter_result =
+	  daw::json::from_json<daw::twitter::twitter_object_t>( json_sv1 );
+	daw::do_not_optimize( twitter_result );
+	daw_json_assert( twitter_result.statuses.size( ) > 0, "Expected values" );
+	daw_json_assert( twitter_result.statuses.front( ).user.id == "1186275104",
+	                 "Missing value" );
+} catch( daw::json::json_exception const &jex ) {
+	std::cerr << "Exception thrown by parser: " << jex.reason( ) << std::endl;
+	exit( 1 );
+}
