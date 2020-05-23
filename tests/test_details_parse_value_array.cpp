552b31dea69347975a6733c8ba98f97b9dd49db5
Darrell Wright <beached@users.noreply.github.com>
2020-05-23 16:58:41 -0400

    Added another parse_value array test

--- tests/test_details_parse_value_array.cpp
+++ tests/test_details_parse_value_array.cpp
@@ -27,6 +27,19 @@ bool empty_array_empty_json_array( ) {
 	return v.empty( );
 }
 
+bool int_array_json_string_array_fail( ) {
+	using namespace daw::json;
+	using namespace daw::json::json_details;
+
+	std::string_view sv = R"([ "this is strange" ])";
+	daw::do_not_optimize( sv );
+	auto rng = IteratorRange( sv.data( ), sv.data( ) + sv.size( ) );
+	auto v = parse_value<json_array<no_name, int>>(
+	  ParseTag<JsonParseTypes::Array>{ }, rng );
+	daw::do_not_optimize( v );
+	return true;
+}
+
 #define do_test( ... )                                                         \
 	try {                                                                        \
 		daw::expecting_message( __VA_ARGS__, "" #__VA_ARGS__ );                    \
@@ -48,6 +61,7 @@ bool empty_array_empty_json_array( ) {
 
 int main( int, char ** ) try {
 	do_test( empty_array_empty_json_array( ) );
+	do_fail_test( int_array_json_string_array_fail( ) );
 } catch( daw::json::json_exception const &jex ) {
 	std::cerr << "Exception thrown by parser: " << jex.reason( ) << std::endl;
 	exit( 1 );
