1d9a669c57a5cb9f226e078a422b43571f4f549f
Darrell Wright <beached@users.noreply.github.com>
2020-06-12 21:33:21 -0400

    added fast path when string parsing uses default std::string types and default appenders

--- include/daw/json/impl/daw_json_parse_value.h
+++ include/daw/json/impl/daw_json_parse_value.h
@@ -19,6 +19,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <iterator>
 #include <tuple>
 
 namespace daw::json::json_details {
@@ -357,6 +358,94 @@ namespace daw::json::json_details {
 		return { };
 	}
 
+	// Fast path for parsing escaped strings to a std::string with the default
+	// appender
+	template<typename JsonMember, typename Range>
+	[[nodiscard, maybe_unused]] inline std::string
+	parse_string_known_stdstring( Range &rng ) {
+		static constexpr EightBitModes eight_bit_mode = JsonMember::eight_bit_mode;
+
+		auto result = std::string( );
+		result.resize( rng.size( ) );
+		auto it = result.data( );
+		auto app = [&]( char c ) {
+			*it = c;
+			++it;
+		};
+
+		bool const has_quote = rng.front( '"' );
+		if( has_quote ) {
+			rng.remove_prefix( );
+		}
+		daw_json_assert_weak( not rng.empty( ), "Unexpected end of data" );
+		while( rng.front( ) != '"' ) {
+			while( rng.front( ) != '"' and rng.front( ) != '\\' ) {
+				daw_json_assert_weak( not rng.empty( ), "Unexpected end of data" );
+				app( rng.front( ) );
+				rng.remove_prefix( );
+			}
+			if( rng.front( ) == '\\' ) {
+				daw_json_assert_weak( rng.front( ) >= 0x20, "Invalid codepoint" );
+				rng.remove_prefix( );
+				switch( rng.front( ) ) {
+				case 'b':
+					app( '\b' );
+					rng.remove_prefix( );
+					break;
+				case 'f':
+					app( '\f' );
+					rng.remove_prefix( );
+					break;
+				case 'n':
+					app( '\n' );
+					rng.remove_prefix( );
+					break;
+				case 'r':
+					app( '\r' );
+					rng.remove_prefix( );
+					break;
+				case 't':
+					app( '\t' );
+					rng.remove_prefix( );
+					break;
+				case 'U': // Sometimes people put crap
+				case 'u':
+					decode_utf16( rng, app );
+					break;
+				case '\\':
+				case '/':
+				case '"':
+					app( rng.front( ) );
+					rng.remove_prefix( );
+					break;
+				default:
+					if constexpr( eight_bit_mode == EightBitModes::DisallowHigh ) {
+						daw_json_assert_weak(
+						  static_cast<unsigned>( rng.front( ) ) >= 0x20U and
+						    static_cast<unsigned>( rng.front( ) ) <= 0x7FU,
+						  "string support limited to 0x20 < chr <= 0x7F when "
+						  "DisallowHighEightBit is true" );
+					}
+					app( rng.front( ) );
+					rng.remove_prefix( );
+				}
+			} else {
+				daw_json_assert_weak( not has_quote or rng.front( '"' ),
+				                      "Unexpected end of string" );
+			}
+			daw_json_assert_weak( not has_quote or rng.has_more( ),
+			                      "Unexpected end of data" );
+		}
+		daw_json_assert_weak(
+		  result.size( ) <
+		    static_cast<std::size_t>( std::distance( result.data( ), it ) ),
+		  "Unexpected string state" );
+		result.resize(
+		  static_cast<std::size_t>( std::distance( result.data( ), it ) ) );
+		return result;
+	}
+
+	//**************************
 	template<typename JsonMember, bool KnownBounds, typename Range>
 	[[nodiscard, maybe_unused]] constexpr json_result<JsonMember>
 	parse_value( ParseTag<JsonParseTypes::StringEscaped>, Range &rng ) {
@@ -369,6 +458,10 @@ namespace daw::json::json_details {
 		if constexpr( std::is_same_v<std::string, daw::remove_cvref_t<decltype(
 		                                            is_string( result ) )>> ) {
 			if( KnownBounds ) {
+				if constexpr( std::is_same_v<appender_t,
+				                             basic_appender<std::string>> ) {
+					return parse_string_known_stdstring<JsonMember>( rng );
+				}
 				result.reserve( rng.size( ) );
 			}
 		}
