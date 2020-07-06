bf79acf72cb47063bb1f902cf48cc62e3d936e9c
Darrell Wright <beached@users.noreply.github.com>
2020-07-06 13:42:12 -0400

    Small clarification to test

--- tests/canada_test_basic.cpp
+++ tests/canada_test_basic.cpp
@@ -22,12 +22,10 @@ int main( int argc, char **argv ) try {
 		exit( 1 );
 	}
 	using namespace daw::json;
-	auto const json_data1 = daw::filesystem::memory_mapped_file_t<>( argv[1] );
-	auto const json_sv1 =
-	  std::string_view( json_data1.data( ), json_data1.size( ) );
+	auto json_data = std::string(daw::filesystem::memory_mapped_file_t<>( argv[1] ) );
 
 	auto const canada_result =
-	  daw::json::from_json<daw::geojson::FeatureCollection>( json_sv1 );
+	  daw::json::from_json<daw::geojson::FeatureCollection>( json_data );
 	daw::do_not_optimize( canada_result );
 
 	auto const new_json_result = daw::json::to_json( canada_result );
