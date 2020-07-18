187c50c547884a87a5b9465e0fb967193fa59e19
Darrell Wright <beached@users.noreply.github.com>
2020-07-18 16:53:25 -0400

    Added clangcl as a CI build type

---
+++ ci_scripts/ci_windows_deps_clangcl.cmd
@@ -0,0 +1,12 @@
+echo "Making build directory"
+md build
+cd build
+
+REM ##############################
+REM Setting VCVars
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+REM ##############################
+REM Running cmake
+cmake -DCMAKE_BUILD_TYPE=%1 -GNinja ..
+
