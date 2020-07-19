22589cfbe57cb4438ed51eaf255dc1de7e903a98
Darrell Wright <beached@users.noreply.github.com>
2020-07-18 21:12:44 -0400

    missing space

--- ci_scripts/ci_windows_deps_clangcl.cmd
+++ ci_scripts/ci_windows_deps_clangcl.cmd
@@ -12,5 +12,5 @@ REM SET PATH=%PATH%;C:\Program Files\LLVM\bin\
 
 REM ##############################
 REM Running cmake
-cmake -DCMAKE_BUILD_TYPE=%1 -GNinja -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe"..
+cmake -DCMAKE_BUILD_TYPE=%1 -GNinja -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" ..
 
