cedcbb870a3382d9ddff076da3bc13a3d4d8704a
Darrell Wright <beached@users.noreply.github.com>
2020-07-18 21:07:04 -0400

    trying full path

--- ci_scripts/ci_windows_deps_clangcl.cmd
+++ ci_scripts/ci_windows_deps_clangcl.cmd
@@ -9,9 +9,8 @@ REM ##############################
 REM Setting VCVars
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 REM SET PATH=%PATH%;C:\Program Files\LLVM\bin\
-SET PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\Llvm\x64\bin"
 
 REM ##############################
 REM Running cmake
-cmake -DCMAKE_BUILD_TYPE=%1 -GNinja -DCMAKE_CXX_COMPILER=clang-cl.exe -DCMAKE_C_COMPILER=clang-cl.exe ..
+cmake -DCMAKE_BUILD_TYPE=%1 -GNinja -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe"..
 
