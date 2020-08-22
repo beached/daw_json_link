8f3cc9287563e48967e8219a40d6a29961a9595c
Darrell Wright <beached@users.noreply.github.com>
2020-08-22 17:22:47 -0400

    Set CI's to only do 1 run in benchmarks to speed them up

--- ci_scripts/ci_macos_deps.sh
+++ ci_scripts/ci_macos_deps.sh
@@ -5,7 +5,7 @@ mkdir build
 cd build
 
 echo "Configuring project"
-cmake -DCMAKE_BUILD_TYPE=$1 ..
+cmake -DCMAKE_BUILD_TYPE=$1 -DDAW_NUM_RUNS=1 ..
 
 echo "Starting dep build"
 cmake --build . --config $1 --target -j 2
