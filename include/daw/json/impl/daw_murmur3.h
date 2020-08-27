50cfd44c7ed90f608326a204b281a7e7e007aef0
Darrell Wright <beached@users.noreply.github.com>
2020-08-27 15:55:19 -0400

    fixed rotate left in murmur3, improved codegen

--- include/daw/json/impl/daw_murmur3.h
+++ include/daw/json/impl/daw_murmur3.h
@@ -24,35 +24,39 @@ namespace daw {
 			return result;
 		}
 
+		static inline constexpr uint32_t rotl( std::uint32_t n, unsigned c ) {
+			constexpr unsigned mask = ( CHAR_BIT * sizeof( n ) - 1 );
+			c &= mask;
+			return ( n << c ) | ( n >> ( ( -c ) & mask ) );
+		}
+
 		inline constexpr std::uint32_t
 		murmur3_32_scramble( std::uint32_t k ) noexcept {
 			k *= 0xcc9e'2d51ULL;
-			k = ( k << 15U ) | ( k >> 17U );
+			rotl( k, 15 );
 			k *= 0x1b87'3593ULL;
 			return k;
 		}
 	} // namespace murmur3_details
 
-	template<typename StringView>
-	constexpr std::uint32_t murmur3_32( StringView key,
-	                                    std::uint32_t seed = 0 ) noexcept {
-		std::uint32_t h = seed;
-		std::uint32_t k = 0;
+	template<typename String>
+	constexpr std::uint32_t murmur3_32( String const &key ) noexcept {
+		std::uint32_t h = 0; // seed
 		auto const len = static_cast<uint32_t>( key.size( ) );
 		char const *first = key.data( );
-		char const *const last = key.data( ) + key.size( );
+		char const *const last = key.data( ) + len;
 		while( ( last - first ) >= 4U ) {
 			// Here is a source of differing results across endiannesses.
 			// A swap here has no effects on hash properties though.
-			k = murmur3_details::to_u32( first );
+			std::uint32_t k = murmur3_details::to_u32( first );
 			h ^= murmur3_details::murmur3_32_scramble( k );
-			h = ( h << 13U ) | ( h >> 19U );
+			h = murmur3_details::rotl( h, 13 );
 			h = h * 5U + 0xe654'6b64ULL;
 			first += 4;
 		}
 
 		// Anything left over
-		k = 0U;
+		std::uint32_t k = 0U;
 		for( auto d = ( last - first ); d > 0; --d ) {
 			k <<= 8U;
 			char const c = *( first + ( d - 1 ) );
