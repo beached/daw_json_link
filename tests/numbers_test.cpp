c2ec48c0cd7550401c97201198a811e7ae00f5d0
Darrell Wright <beached@users.noreply.github.com>
2020-08-31 12:43:12 -0400

    Added 3 exec tags as tests in numbers

--- tests/numbers_test.cpp
+++ tests/numbers_test.cpp
@@ -26,6 +26,42 @@ static inline constexpr std::size_t DAW_NUM_RUNS = 1;
 #endif
 static_assert( DAW_NUM_RUNS > 0 );
 
+template<typename ExecTag>
+void test( std::string_view data ) {
+	std::cout << "Using " << ExecTag::name
+	          << " exec model\n*********************************************\n";
+	using range_t = daw::json::json_array_range<
+	  double, daw::json::SIMDNoCommentSkippingPolicyChecked<ExecTag>>;
+
+	std::vector<double> results = [&] {
+		auto rng = range_t( data );
+		return std::vector<double>( rng.begin( ), rng.end( ) );
+	}( );
+	daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+	  "numbers bench (checked)", data.size( ),
+	  [&]( auto rng ) {
+		  double *ptr = results.data( );
+		  for( double d : rng ) {
+			  *ptr++ += d;
+		  }
+	  },
+	  range_t( data ) );
+	daw::do_not_optimize( results );
+	using range2_t = daw::json::json_array_range<
+	  double, daw::json::SIMDNoCommentSkippingPolicyUnchecked<ExecTag>>;
+	daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+	  "numbers bench (unchecked)", data.size( ),
+	  [&]( auto rng ) {
+		  double *ptr = results.data( );
+		  for( double d : rng ) {
+			  *ptr++ += d;
+		  }
+	  },
+	  range2_t( data ) );
+
+	daw::do_not_optimize( results );
+}
+
 int main( int argc, char **argv ) try {
 	using namespace daw::json;
 	if( argc < 2 ) {
@@ -38,39 +74,15 @@ int main( int argc, char **argv ) try {
 	auto const sv_numbers =
 	  std::string_view( mm_numbers.data( ), mm_numbers.size( ) );
 
+#ifndef NDEBUG
 	std::cout << "non-debug run\n";
-	using range_t = daw::json::json_array_range<
-	  double,
-	  daw::json::SIMDNoCommentSkippingPolicyChecked<daw::json::simd_exec_tag>>;
+#endif
+	test<daw::json::constexpr_exec_tag>( sv_numbers );
+	test<daw::json::runtime_exec_tag>( sv_numbers );
+#if defined( DAW_ALLOW_SSE42 )
+	test<daw::json::sse42_exec_tag>( sv_numbers );
+#endif
 
-	std::vector<double> results = [&] {
-		auto rng = range_t( sv_numbers );
-		return std::vector<double>( rng.begin( ), rng.end( ) );
-	}( );
-	daw::bench_n_test_mbs<DAW_NUM_RUNS>(
-	  "numbers bench (checked)", sv_numbers.size( ),
-	  [&]( auto rng ) {
-		  double *ptr = results.data( );
-		  for( double d : rng ) {
-			  *ptr++ += d;
-		  }
-	  },
-	  range_t( sv_numbers ) );
-	daw::do_not_optimize( results );
-	using range2_t = daw::json::json_array_range<
-	  double,
-	  daw::json::SIMDNoCommentSkippingPolicyUnchecked<daw::json::simd_exec_tag>>;
-	daw::bench_n_test_mbs<DAW_NUM_RUNS>(
-	  "numbers bench (unchecked)", sv_numbers.size( ),
-	  [&]( auto rng ) {
-		  double *ptr = results.data( );
-		  for( double d : rng ) {
-			  *ptr++ += d;
-		  }
-	  },
-	  range2_t( sv_numbers ) );
-
-	daw::do_not_optimize( results );
 } catch( daw::json::json_exception const &jex ) {
 	std::cerr << "Exception thrown by parser: " << jex.reason( ) << std::endl;
 	exit( 1 );
