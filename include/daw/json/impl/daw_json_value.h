e3a78472da38484c369a8409ae92d4d01eb6871c
Darrell Wright <beached@users.noreply.github.com>
2020-09-03 16:27:52 -0400

    Implemented JSON minification for #142

--- include/daw/json/impl/daw_json_value.h
+++ include/daw/json/impl/daw_json_value.h
@@ -208,6 +208,20 @@ namespace daw::json {
 			case '[':
 			case '{':
 			case '"':
+			case '-':
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+			case 't':
+			case 'f':
+			case 'n':
 				return true;
 			}
 			return false;
--- tests/daw_json_minify.cpp
+++ tests/daw_json_minify.cpp
@@ -6,20 +6,14 @@
 // Official repository: https://github.com/beached/daw_json_link
 //
 
-#include "defines.h"
-
 #include "daw/json/daw_json_link.h"
 
 #include <daw/daw_memory_mapped_file.h>
-#include <daw/daw_visit.h>
 
-#include <cstdint>
-#include <cstdio>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <stack>
-#include <string>
 
 template<typename ExecTag, typename OutIt>
 constexpr void minify( std::string_view json_data, OutIt out_it ) {}
@@ -34,8 +28,7 @@ int main( int argc, char **argv ) try {
 	}
 	auto data = daw::filesystem::memory_mapped_file_t<>( argv[1] );
 
-	using Range =
-	  daw::json::SIMDNoCommentSkippingPolicyChecked<daw::json::simd_exec_tag>;
+	constexpr bool VerifyValues = true;
 
 	std::optional<std::ofstream> out_file =
 	  [&]( ) -> std::optional<std::ofstream> {
@@ -46,6 +39,7 @@ int main( int argc, char **argv ) try {
 				std::cerr << "Could not open " << argv[2] << " for writing\n";
 				exit( 1 );
 			}
+			return result;
 		}
 		return { };
 	}( );
@@ -57,85 +51,115 @@ int main( int argc, char **argv ) try {
 		return std::cout;
 	}( );
 
-	using iterator = daw::json::basic_json_value_iterator<Range>;
-	using json_value_t = daw::json::basic_json_pair<Range>;
-	using stack_value_t =
-	  std::variant<std::pair<iterator, iterator>, json_value_t>;
+	using ParsePolicy =
+	  daw::json::SIMDNoCommentSkippingPolicyChecked<daw::json::simd_exec_tag>;
+
+	using iterator = daw::json::basic_json_value_iterator<ParsePolicy>;
+	using json_value_t = daw::json::basic_json_pair<ParsePolicy>;
+	struct stack_value_t {
+		daw::json::JsonBaseParseTypes type;
+		std::pair<iterator, iterator> value;
+		bool is_first = true;
+	};
 	using min_stack_t = std::stack<stack_value_t, std::vector<stack_value_t>>;
 
 	auto parent_stack = min_stack_t( );
 
-	std::function<void( daw::json::basic_json_value<Range> jv )>
-	  process_pair_value = [&]( daw::json::basic_json_value<Range> jv ) {
-		  switch( jv.type( ) ) {
-		  case daw::json::JsonBaseParseTypes::Array: {
-			  outs << '[';
-			  parent_stack.push(
-			    std::pair<iterator, iterator>( jv.begin( ), jv.end( ) ) );
-		  } break;
-		  case daw::json::JsonBaseParseTypes::Class: {
-			  outs << '{';
-			  if( jv.begin( ) == jv.end( ) ) {
-				  outs << '}';
-			  } else {
-				  auto f = jv.begin( );
-				  auto l = jv.end( );
-				  auto p = *f;
-				  if( p.name ) {
-					  outs << '"' << *( p.name ) << "\":";
-				  }
-				  ++f;
-				  parent_stack.push( std::pair<iterator, iterator>( f, l ) );
-				  process_pair_value( p.value );
-			  }
-		  } break;
-		  case daw::json::JsonBaseParseTypes::String:
-			  outs << jv.get_string_view( );
-			  break;
-		  case daw::json::JsonBaseParseTypes::Number:
-		  case daw::json::JsonBaseParseTypes::Bool:
-		  case daw::json::JsonBaseParseTypes::Null:
-			  outs << jv.get_string_view( );
-			  break;
-		  case daw::json::JsonBaseParseTypes::None:
-		  default:
-			  std::cerr << "Unexpected type\n";
-			  std::terminate( );
-		  }
-	  };
-
-	auto const process_value = [&]( json_value_t jv ) {
-		if( jv.name ) {
-			outs << '"' << *jv.name << "\":";
+	auto const process_value = [&]( json_value_t p ) {
+		auto &jv = p.value;
+		if( p.name and not p.name->empty( ) ) {
+			outs << '"' << *p.name << "\":";
 		}
-		process_pair_value( jv.value );
+		switch( jv.type( ) ) {
+		case daw::json::JsonBaseParseTypes::Array:
+			outs << '[';
+			parent_stack.push(
+			  { daw::json::JsonBaseParseTypes::Array,
+			    std::pair<iterator, iterator>( jv.begin( ), jv.end( ) ) } );
+			break;
+		case daw::json::JsonBaseParseTypes::Class: {
+			outs << '{';
+			parent_stack.push(
+			  { daw::json::JsonBaseParseTypes::Class,
+			    std::pair<iterator, iterator>( jv.begin( ), jv.end( ) ) } );
+			break;
+		case daw::json::JsonBaseParseTypes::Number:
+			if constexpr( VerifyValues ) {
+				auto v = jv;
+				(void)daw::json::from_json<daw::json::json_number<daw::json::no_name>,
+				                           ParsePolicy>( v );
+			}
+			outs << jv.get_string_view( );
+			break;
+		case daw::json::JsonBaseParseTypes::Bool:
+			if constexpr( VerifyValues ) {
+				auto v = jv;
+				(void)daw::json::from_json<daw::json::json_bool<daw::json::no_name>,
+				                           ParsePolicy>( v );
+			}
+			outs << jv.get_string_view( );
+			break;
+		case daw::json::JsonBaseParseTypes::String:
+			outs << jv.get_string_view( );
+			break;
+		case daw::json::JsonBaseParseTypes::Null:
+			if constexpr( VerifyValues ) {
+				auto v = jv;
+				(void)daw::json::from_json<
+				  daw::json::json_number_null<daw::json::no_name>, ParsePolicy>( v );
+			}
+			break;
+		case daw::json::JsonBaseParseTypes::None:
+		default:
+			std::cerr << "Unexpected type\n";
+			std::terminate( );
+		}
+		}
 	};
 
-	auto const process_range = [&]( std::pair<iterator, iterator> p ) {
-		if( p.first != p.second ) {
-			auto jv = *p.first;
-			p.first++;
-			if( p.first != p.second ) {
-				parent_stack.push( std::move( p ) );
-			} else {
-				if( jv.name ) {
-					outs << '}';
+	auto const process_range = [&]( stack_value_t v ) {
+		if( v.value.first != v.value.second ) {
+			auto jv = *v.value.first;
+			if( not jv.value.is_null( ) ) {
+				if( v.is_first ) {
+					v.is_first = false;
 				} else {
-					outs << ']';
+					outs << ',';
 				}
 			}
-			outs << ',';
-			process_value( std::move( jv ) );
+			v.value.first++;
+			parent_stack.push( std::move( v ) );
+			if( not jv.value.is_null( ) ) {
+				process_value( std::move( jv ) );
+			}
+		} else {
+			switch( v.type ) {
+			case daw::json::JsonBaseParseTypes::Class:
+				outs << '}';
+				break;
+			case daw::json::JsonBaseParseTypes::Array:
+				outs << ']';
+				break;
+			case daw::json::JsonBaseParseTypes::Number:
+			case daw::json::JsonBaseParseTypes::Bool:
+			case daw::json::JsonBaseParseTypes::String:
+			case daw::json::JsonBaseParseTypes::Null:
+			case daw::json::JsonBaseParseTypes::None:
+			default:
+				std::cerr << "Logic error\n";
+				std::terminate( );
+			}
 		}
 	};
-	parent_stack.push( json_value_t{
-	  std::nullopt, daw::json::basic_json_value<Range>(
-	                  std::string_view( data.data( ), data.size( ) ) ) } );
+
+	process_value(
+	  { std::nullopt, daw::json::basic_json_value<ParsePolicy>(
+	                    std::string_view( data.data( ), data.size( ) ) ) } );
 
 	while( not parent_stack.empty( ) ) {
-		auto v = parent_stack.top( );
+		auto v = std::move( parent_stack.top( ) );
 		parent_stack.pop( );
-		daw::visit_nt( std::move( v ), process_value, process_range );
+		process_range( v );
 	}
 	if( not out_file ) {
 		outs << '\n';
