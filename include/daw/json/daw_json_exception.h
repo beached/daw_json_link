1676c61c6355ab96f11d1755fd07a74879e15c90
Darrell Wright <beached@users.noreply.github.com>
2020-10-15 10:07:44 -0400

    Changed get_message to return a string_view instead of a char const *

--- include/daw/json/daw_json_exception.h
+++ include/daw/json/daw_json_exception.h
@@ -97,96 +97,97 @@ namespace daw::json {
 		UnexpectedJSONVariantType
 	};
 
-	constexpr char const *reason_message( ErrorReason er ) {
+	constexpr std::string_view reason_message( ErrorReason er ) {
+		using namespace std::string_view_literals;
 		switch( er ) {
 		case ErrorReason::Unknown:
-			return "Unknown reason for error";
+			return "Unknown reason for error"sv;
 		case ErrorReason::UnexpectedEndOfData:
-			return "Unexpected end of data";
+			return "Unexpected end of data"sv;
 		case ErrorReason::InvalidNumber:
-			return "Invalid Number";
+			return "Invalid Number"sv;
 		case ErrorReason::InvalidNumberStart:
-			return "Invalid Number started, expected a \"0123456789-\"";
+			return R"(Invalid Number started, expected a "0123456789-")"sv;
 		case ErrorReason::InvalidNumberUnexpectedQuoting:
-			return "Unexpected double quote prior to number";
+			return "Unexpected double quote prior to number"sv;
 		case ErrorReason::InvalidTimestamp:
-			return "Invalid Timestamp";
+			return "Invalid Timestamp"sv;
 		case ErrorReason::InvalidUTFEscape:
-			return "Invalid UTF Escape";
+			return "Invalid UTF Escape"sv;
 		case ErrorReason::InvalidUTFCodepoint:
-			return "Invalid UTF Codepoint";
+			return "Invalid UTF Codepoint"sv;
 		case ErrorReason::InvalidEndOfValue:
-			return "Did not find \",}]\" at end of value";
+			return R"(Did not find \",}]" at end of value)"sv;
 		case ErrorReason::InvalidLiteral:
-			return "Literal data was corrupt";
+			return "Literal data was corrupt"sv;
 		case ErrorReason::InvalidString:
-			return "Invalid or corrupt string";
+			return "Invalid or corrupt string"sv;
 		case ErrorReason::InvalidStringHighASCII:
 			return "String support limited to 0x20 < chr <= 0x7F when "
-			       "DisallowHighEightBit is true";
+			       "DisallowHighEightBit is true"sv;
 		case ErrorReason::ExpectedKeyValueToStartWithBrace:
 			return "Expected key/value's JSON type to be of class type and beginning "
-			       "with '{'";
+			       "with '{'"sv;
 		case ErrorReason::ExpectedKeyValueArrayToStartWithBracket:
 			return "Expected key/value's JSON type to be of array type and beginning "
-			       "with '['";
+			       "with '['"sv;
 		case ErrorReason::InvalidArrayStart:
-			return "Expected array type to begin with '['";
+			return "Expected array type to begin with '['"sv;
 		case ErrorReason::InvalidClassStart:
-			return "Expected class type to begin with '{'";
+			return "Expected class type to begin with '{'"sv;
 		case ErrorReason::InvalidStartOfValue:
-			return "Unexpected character data at start of value";
+			return "Unexpected character data at start of value"sv;
 		case ErrorReason::InvalidTrue:
-			return "Expected true not found";
+			return "Expected true not found"sv;
 		case ErrorReason::InvalidFalse:
-			return "Expected false not found";
+			return "Expected false not found"sv;
 		case ErrorReason::InvalidNull:
-			return "Expected null not found";
+			return "Expected null not found"sv;
 		case ErrorReason::UnexpectedNull:
-			return "An unexpected null value was encountered while serializing";
+			return "An unexpected null value was encountered while serializing"sv;
 		case ErrorReason::NumberIsNaN:
-			return "NaN encountered while serializing to JSON Number literal";
+			return "NaN encountered while serializing to JSON Number literal"sv;
 		case ErrorReason::NumberIsInf:
-			return "Infinity encountered while serializing to JSON Number literal";
+			return "Infinity encountered while serializing to JSON Number literal"sv;
 		case ErrorReason::NumberOutOfRange:
-			return "Number is outside of the representable range";
+			return "Number is outside of the representable range"sv;
 		case ErrorReason::EmptyJSONDocument:
-			return "Attempt to parse an empty JSON document";
+			return "Attempt to parse an empty JSON document"sv;
 		case ErrorReason::EmptyJSONPath:
-			return "Empty JSON Path specified";
+			return "Empty JSON Path specified"sv;
 		case ErrorReason::JSONPathNotFound:
-			return "JSON Path specified not found in document";
+			return "JSON Path specified not found in document"sv;
 		case ErrorReason::InvalidJSONPath:
-			return "Invalid JSON Path specified";
+			return "Invalid JSON Path specified"sv;
 		case ErrorReason::NullOutputIterator:
-			return "Null pointer specified for output";
+			return "Null pointer specified for output"sv;
 		case ErrorReason::MissingMemberNameOrEndOfClass:
-			return "Missing member name or end of class";
+			return "Missing member name or end of class"sv;
 		case ErrorReason::MissingMemberName:
-			return "Missing member name";
+			return "Missing member name"sv;
 		case ErrorReason::InvalidMemberName:
-			return "Member names must be JSON strings";
+			return "Member names must be JSON strings"sv;
 		case ErrorReason::ExpectedArrayOrClassStart:
-			return "Expected start of a JSON class or array";
+			return "Expected start of a JSON class or array"sv;
 		case ErrorReason::UnknownMember:
-			return "Could not find member in JSON class";
+			return "Could not find member in JSON class"sv;
 		case ErrorReason::InvalidBracketing:
-			return "Invalid Bracketing";
+			return "Invalid Bracketing"sv;
 		case ErrorReason::AttemptToAccessPastEndOfValue:
-			return "A value of known size was accessed past the end";
+			return "A value of known size was accessed past the end"sv;
 		case ErrorReason::OutOfOrderOrderedMembers:
-			return "Order of ordered members must be ascending";
+			return "Order of ordered members must be ascending"sv;
 		case ErrorReason::MemberNotFound:
-			return "Expected member not found";
+			return "Expected member not found"sv;
 		case ErrorReason::TagMemberNotFound:
 			return "Expected tag member not found, they are required for tagged "
-			       "Variants";
+			       "Variants"sv;
 		case ErrorReason::ExpectedMemberNotFound:
-			return "Expected member missing";
+			return "Expected member missing"sv;
 		case ErrorReason::ExpectedTokenNotFound:
-			return "Expected token missing";
+			return "Expected token missing"sv;
 		case ErrorReason::UnexpectedJSONVariantType:
-			return "Unexpected JSON Variant Type";
+			return "Unexpected JSON Variant Type"sv;
 		}
 		DAW_JSON_UNREACHABLE( );
 	}
