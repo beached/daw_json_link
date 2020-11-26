a9cd4f8801d27dc6749d0177c329dbbcceb2b569
Darrell Wright <beached@users.noreply.github.com>
2020-11-26 09:58:53 -0500

    cleaned up some cast issues to be explicit

--- include/daw/json/impl/daw_json_parse_real.h
+++ include/daw/json/impl/daw_json_parse_real.h
@@ -305,12 +305,13 @@ namespace daw::json::json_details {
 				first = skip_digits<Range::is_unchecked_input>( first, rng.last );
 			} else {
 				char const *fract_last =
-				  first +
-				  std::min( rng.last - first, max_exponent - ( first - rng.first ) );
+				  first + std::min( rng.last - first,
+				                    static_cast<std::ptrdiff_t>(
+				                      max_exponent - ( first - rng.first ) ) );
 
 				last_char = parse_real_digits_while_number<Range::is_unchecked_input>(
 				  first, fract_last, significant_digits );
-				exponent -= last_char - first;
+				exponent -= static_cast<signed_t>( last_char - first );
 				first = last_char;
 				if( first >= fract_last ) {
 					first = skip_digits<Range::is_unchecked_input>( first, rng.last );
@@ -344,11 +345,11 @@ namespace daw::json::json_details {
 				  first, rng.last, exp_tmp );
 				first = last_char;
 				if( exp_sign ) {
-					return -static_cast<unsigned_t>( exp_tmp );
+					return -static_cast<signed_t>( exp_tmp );
 				}
-				return static_cast<unsigned_t>( exp_tmp );
+				return static_cast<signed_t>( exp_tmp );
 			}
-			return static_cast<unsigned_t>( 0 );
+			return static_cast<signed_t>( 0 );
 		}( );
 		rng.first = first;
 		return sign * power10<Result>( Range::exec_tag,
