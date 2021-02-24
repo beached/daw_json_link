89ab9ef63de4b766c28d603f249b493ee67a13b5
Darrell Wright <beached@users.noreply.github.com>
2021-02-24 09:55:05 -0500

    fixed typo

--- ci_scripts/ci_windows_deps_clangcl.cmd
+++ ci_scripts/ci_windows_deps_clangcl.cmd
@@ -8,6 +8,6 @@ REM Setting VCVars
 
 REM ##############################
 REM Running cmake
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/COMMON7/COMMONEXTENSIONS/MICROSOFT/CMAKE/CMake/bin/cmake.exe" -DCMAKE_BUILD_TYPE=%1 -GNinja -DDAW_ENABLE_TESTING=On -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" ..
+"C:/PROGRAM FILES (X86)/MICROSOFT VISUAL STUDIO/2019/ENTERPRISE/COMMON7/IDE/COMMONEXTENSIONS/MICROSOFT/CMAKE/CMake/bin/cmake.exe" -DCMAKE_BUILD_TYPE=%1 -GNinja -DDAW_ENABLE_TESTING=On -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/bin/clang-cl.exe" ..
 
 
