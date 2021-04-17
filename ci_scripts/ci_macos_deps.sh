1eb5ae6a1ebc6035711cb2632c932212b070f99d
Darrell Wright <beached@users.noreply.github.com>
2021-04-17 13:46:07 -0400

    Matching CI from develop

--- ci_scripts/ci_macos_deps.sh
+++ ci_scripts/ci_macos_deps.sh
@@ -5,7 +5,7 @@ mkdir build
 cd build
 
 echo "Configuring project"
-cmake -DCMAKE_BUILD_TYPE=$1 -DDAW_NUM_RUNS=1 -DDAW_ENABLE_TESTING=On ..
+cmake -DCMAKE_BUILD_TYPE=$1 -DDAW_NUM_RUNS=1 -DDAW_ENABLE_TESTING=ON -DDAW_WERROR=ON ..
 
 echo "Starting dep build"
 cmake --build . --config $1 --target -j 2
