e091b52832c2538703329ec4ab447f5202479dab
Darrell Wright <beached@users.noreply.github.com>
2021-08-10 13:54:56 -0400

    moved ubuntu test to gcc

--- ci_scripts/ci_ubuntu_deps.sh
+++ ci_scripts/ci_ubuntu_deps.sh
@@ -5,7 +5,8 @@ mkdir build
 cd build
 
 echo "Configuring project"
-CC=clang-12 CXX=clang++-12 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=$1 -DDAW_NUM_RUNS=1 -DDAW_ENABLE_TESTING=ON -DDAW_WERROR=ON -DCMAKE_CXX_STANDARD=$2 ..
+#CC=clang-12 CXX=clang++-12 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=$1 -DDAW_NUM_RUNS=1 -DDAW_ENABLE_TESTING=ON -DDAW_WERROR=ON -DCMAKE_CXX_STANDARD=$2 ..
+cmake -DCMAKE_BUILD_TYPE=$1 -DDAW_NUM_RUNS=1 -DDAW_ENABLE_TESTING=ON -DDAW_WERROR=ON -DCMAKE_CXX_STANDARD=$2 ..
 
 echo "Starting dep build"
 cmake --build . --config $1 -j 2
