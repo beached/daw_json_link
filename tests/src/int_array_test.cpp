ccba3dde64dbdd67199553901ca60cceb91a8251
Darrell Wright <beached@users.noreply.github.com>
2021-08-14 21:14:05 -0400

    Fixed error checking on int_array_testing

--- tests/src/int_array_test.cpp
+++ tests/src/int_array_test.cpp
@@ -482,7 +482,17 @@ int main( int argc, char ** )
 	} else {
 		test_func<1'000ULL>( );
 	}
-} catch( daw::json::json_exception const &jex ) {
+}
+#ifdef DAW_USE_EXCEPTIONS
+catch( daw::json::json_exception const &jex ) {
 	std::cerr << "Exception thrown by parser: " << jex.reason( ) << std::endl;
 	exit( 1 );
+} catch( std::exception const &ex ) {
+	std::cerr << "Unknown exception thrown during testing: " << ex.what( )
+	          << '\n';
+	exit( 1 );
+} catch( ... ) {
+	std::cerr << "Unknown exception thrown during testing\n";
+	throw;
 }
+#endif
