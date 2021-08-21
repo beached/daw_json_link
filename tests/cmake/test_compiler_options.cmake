53ec570e01355f08060a650c4b36282c3250f50e
Darrell Wright <beached@users.noreply.github.com>
2021-08-20 22:56:10 -0400

    Filtered clang-9 from -Wno-poison-system-directories warning.

--- tests/cmake/test_compiler_options.cmake
+++ tests/cmake/test_compiler_options.cmake
@@ -44,7 +44,7 @@ if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang" OR ${CMAKE_CXX_COMPILER_ID} STREQU
                 # This is for when specializing thigns like tuple_size and each implementer gets to choose struct/class
                 -Wno-mismatched-tags
         )
-        if (${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang" OR CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 8.0.0)
+        if (${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang" OR CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 10.0.0)
             add_compile_options(-Wno-poison-system-directories)
         endif ()
         if (DAW_WERROR)
