b5b989bb11107b49c24bf9e74661a4aecad8d976
Darrell Wright <beached@users.noreply.github.com>
2021-08-20 23:59:06 -0400

    Fixed exception checking in strings_test

--- tests/src/strings_test.cpp
+++ tests/src/strings_test.cpp
@@ -162,7 +162,17 @@ int main( int argc, char **argv )
 		auto const h2 = test<daw::json::simd_exec_tag>( json_string );
 		test_equal( h0, h2, "constexpr/fast exec model hashes do not match" );
 	}
-} catch( daw::json::json_exception const &jex ) {
-	std::cerr << "Exception thrown by parser: " << jex.reason( ) << std::endl;
-	exit( 1 );
 }
+#ifdef DAW_USE_EXCEPTIONS
+catch( daw::json::json_exception const &jex ) {
+	std::cerr << "Exception thrown by parser: " << jex.reason( ) << '\n';
+	exit( 1 );
+} catch( std::exception const &ex ) {
+	std::cerr << "Unknown exception thrown during testing: " << ex.what( )
+	          << '\n';
+	exit( 1 );
+} catch( ... ) {
+	std::cerr << "Unknown exception thrown during testing\n";
+	throw;
+}
+#endif
