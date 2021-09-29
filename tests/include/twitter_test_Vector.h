4196ed833813f876f33b08f2ecc251a23e6a1fee
Darrell Wright <beached@users.noreply.github.com>
2021-09-29 16:54:40 -0400

    Added test of daw::Vector with resize allocator support

---
+++ tests/include/twitter_test_Vector.h
@@ -0,0 +1,263 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/daw_json_link
+//
+
+#pragma once
+
+#include <daw/json/daw_from_json_fwd.h>
+
+#include <chrono>
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace daw::twitter {
+	using twitter_tp = std::chrono::time_point<std::chrono::system_clock,
+	                                           std::chrono::milliseconds>;
+
+	struct metadata_t {
+		std::string result_type;
+		std::string iso_language_code;
+	}; // metadata_t
+
+	struct urls_element_t {
+		std::string url;
+		std::string expanded_url;
+		std::string display_url;
+		std::vector<int32_t> indices;
+	}; // urls_element_t
+
+	struct url_t {
+		std::vector<urls_element_t> urls;
+	}; // url_t
+
+	struct entities_t {
+		std::optional<url_t> url;
+		std::optional<url_t> description;
+	}; // entities_t
+
+	struct user_t {
+		int64_t id;
+		std::string id_str;
+		std::string name;
+		std::string screen_name;
+		std::string location;
+		std::string description;
+		std::optional<std::string> url;
+		entities_t entities;
+		bool _jsonprotected;
+		int32_t followers_count;
+		int32_t friends_count;
+		int32_t listed_count;
+		twitter_tp created_at;
+		int32_t favourites_count;
+		bool geo_enabled;
+		bool verified;
+		int32_t statuses_count;
+		std::string lang;
+		bool contributors_enabled;
+		bool is_translator;
+		bool is_translation_enabled;
+		std::string profile_background_color;
+		std::string profile_background_image_url;
+		std::string profile_background_image_url_https;
+		bool profile_background_tile;
+		std::string profile_image_url;
+		std::string profile_image_url_https;
+		std::optional<std::string> profile_banner_url;
+		std::string profile_link_color;
+		std::string profile_sidebar_border_color;
+		std::string profile_sidebar_fill_color;
+		std::string profile_text_color;
+		bool profile_use_background_image;
+		bool default_profile;
+		bool default_profile_image;
+		bool following;
+		bool follow_request_sent;
+		bool notifications;
+	}; // user_t
+
+	struct hashtags_element_t {
+		std::string text;
+		std::vector<int32_t> indices;
+	}; // hashtags_element_t
+
+	struct tweet_object_t {
+		metadata_t metadata;
+		twitter_tp created_at;
+		int64_t id;
+		std::string id_str;
+		std::string text;
+		std::string source;
+		bool truncated;
+		std::optional<int64_t> in_reply_to_status_id;
+		std::optional<std::string> in_reply_to_status_id_str;
+		std::optional<int64_t> in_reply_to_user_id;
+		std::optional<std::string> in_reply_to_user_id_str;
+		std::optional<std::string> in_reply_to_screen_name;
+		user_t user;
+		int32_t retweet_count;
+		std::optional<int32_t> favorite_count;
+		entities_t entities;
+		bool favorited;
+		bool retweeted;
+		std::optional<bool> possibly_sensitive;
+		std::string lang;
+	}; // statuses_element_t
+
+	struct user_mentions_element_t {
+		std::string screen_name;
+		std::string name;
+		int64_t id;
+		std::string id_str;
+		std::vector<int32_t> indices;
+	}; // user_mentions_element_t
+
+	struct size_item_t {
+		int64_t w;
+		int64_t h;
+		std::string resize;
+	}; // size_item_t
+
+	struct sizes_t {
+		size_item_t medium;
+		size_item_t small_;
+		size_item_t thumb;
+		size_item_t large;
+	}; // sizes_t
+
+	struct media_element_t {
+		int64_t id;
+		std::string id_str;
+		std::vector<int32_t> indices;
+		std::string media_url;
+		std::string media_url_https;
+		std::string url;
+		std::string display_url;
+		std::string expanded_url;
+		std::string type;
+		sizes_t sizes;
+	}; // media_element_t
+
+	struct retweeted_status_t {
+		metadata_t metadata;
+		twitter_tp created_at;
+		int64_t id;
+		std::string id_str;
+		std::string text;
+		std::string source;
+		bool truncated;
+		std::optional<int64_t> in_reply_to_status_id;
+		std::optional<std::string> in_reply_to_status_id_str;
+		std::optional<int64_t> in_reply_to_user_id;
+		std::optional<std::string> in_reply_to_user_id_str;
+		std::optional<std::string> in_reply_to_screen_name;
+		user_t user;
+		int32_t retweet_count;
+		std::optional<int32_t> favorite_count;
+		entities_t entities;
+		bool favorited;
+		bool retweeted;
+		std::optional<bool> possibly_sensitive;
+		std::string lang;
+	}; // retweeted_status_t
+
+	struct search_metadata_t {
+		double completed_in;
+		int64_t max_id;
+		std::string max_id_str;
+		std::string next_results;
+		std::string query;
+		std::string refresh_url;
+		int64_t count;
+		int64_t since_id;
+		std::string since_id_str;
+	}; // search_metadata_t
+
+	struct twitter_object_t {
+		std::vector<tweet_object_t> statuses;
+		search_metadata_t search_metadata;
+	}; // twitter_object_t
+} // namespace daw::twitter
+
+#if not defined( DAW_JSON_TEST_NO_EXTERN_TEMPLATE )
+namespace daw::json {
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyChecked<constexpr_exec_tag>, false,
+	          daw::twitter::twitter_object_t>( std::string_view const &json_data,
+	                                           std::string_view path );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyUnchecked<constexpr_exec_tag>, false,
+	          daw::twitter::twitter_object_t>( std::string_view const &json_data,
+	                                           std::string_view path );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyChecked<runtime_exec_tag>, false,
+	          daw::twitter::twitter_object_t>( std::string_view const &json_data,
+	                                           std::string_view path );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyUnchecked<runtime_exec_tag>, false,
+	          daw::twitter::twitter_object_t>( std::string_view const &json_data,
+	                                           std::string_view path );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyChecked<simd_exec_tag>, false,
+	          daw::twitter::twitter_object_t>( std::string_view const &json_data,
+	                                           std::string_view path );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyUnchecked<simd_exec_tag>, false,
+	          daw::twitter::twitter_object_t>( std::string_view const &json_data,
+	                                           std::string_view path );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyChecked<constexpr_exec_tag>, false,
+	          daw::twitter::twitter_object_t>(
+	  std::string_view const &json_data );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyUnchecked<constexpr_exec_tag>, false,
+	          daw::twitter::twitter_object_t>(
+	  std::string_view const &json_data );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyChecked<runtime_exec_tag>, false,
+	          daw::twitter::twitter_object_t>(
+	  std::string_view const &json_data );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyUnchecked<runtime_exec_tag>, false,
+	          daw::twitter::twitter_object_t>(
+	  std::string_view const &json_data );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyChecked<simd_exec_tag>, false,
+	          daw::twitter::twitter_object_t>(
+	  std::string_view const &json_data );
+
+	extern template daw::twitter::twitter_object_t
+	from_json<daw::twitter::twitter_object_t,
+	          SIMDNoCommentSkippingPolicyUnchecked<simd_exec_tag>, false,
+	          daw::twitter::twitter_object_t>(
+	  std::string_view const &json_data );
+} // namespace daw::json
+#endif
---
+++ tests/src/twitter_test_Vector.cpp
@@ -0,0 +1,246 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/daw_json_link
+//
+
+#include "defines.h"
+
+#include "twitter_test_Vector_json.h"
+
+#include <daw/cpp_17.h>
+#include <daw/daw_benchmark.h>
+#include <daw/daw_read_file.h>
+#include <daw/daw_traits.h>
+#include <daw/json/daw_from_json.h>
+#include <daw/json/daw_to_json.h>
+
+#include <iostream>
+#include <streambuf>
+
+#if not defined( DAW_NUM_RUNS )
+#if not defined( DEBUG ) or defined( NDEBUG )
+static inline constexpr std::size_t DAW_NUM_RUNS = 250;
+#else
+static inline constexpr std::size_t DAW_NUM_RUNS = 2;
+#endif
+#endif
+static_assert( DAW_NUM_RUNS > 0 );
+
+inline namespace {
+	template<typename ExecTag>
+	void test( std::string_view json_data )
+#ifdef DAW_USE_EXCEPTIONS
+	  try
+#endif
+	{
+		auto const sz = json_data.size( );
+		std::cout << "Using " << ExecTag::name
+		          << " exec model\n*********************************************\n";
+		std::optional<daw::twitter::twitter_object_t> twitter_result;
+		// ******************************
+		// NoCommentSkippingPolicyChecked
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(checked)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDNoCommentSkippingPolicyChecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+
+		// NoCommentSkippingPolicyUnchecked
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(unchecked)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDNoCommentSkippingPolicyUnchecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+
+		// CppCommentSkippingPolicyChecked
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(cpp comments)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDCppCommentSkippingPolicyChecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+#if not defined( _MSC_VER ) or defined( __clang__ )
+		// CppCommentSkippingPolicyUnchecked
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(cpp comments, unchecked)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDCppCommentSkippingPolicyUnchecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+#endif
+		// HashCommentSkippingPolicyChecked
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(hash comments)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDHashCommentSkippingPolicyChecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+#if not defined( _MSC_VER ) or defined( __clang__ )
+		// HashCommentSkippingPolicyUnchecked
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(hash comments, unchecked)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDHashCommentSkippingPolicyUnchecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+#endif
+		// ******************************
+		// NoCommentSkippingPolicyChecked Escaped Names
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(checked, escaped names)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDNoCommentSkippingPolicyChecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+
+		// NoCommentSkippingPolicyUnchecked Escaped Names
+		daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+		  "twitter bench(unchecked, escaped names)", sz,
+		  [&twitter_result]( auto f1 ) {
+			  twitter_result = daw::json::from_json<
+			    daw::twitter::twitter_object_t,
+			    daw::json::SIMDNoCommentSkippingPolicyUnchecked<ExecTag>>( f1 );
+			  daw::do_not_optimize( twitter_result );
+		  },
+		  json_data );
+		daw::do_not_optimize( twitter_result );
+		test_assert( twitter_result, "Missing value" );
+		test_assert( not twitter_result->statuses.empty( ), "Expected values" );
+		test_assert( twitter_result->statuses.front( ).user.id == 1186275104,
+		             "Missing value" );
+	}
+#ifdef DAW_USE_EXCEPTIONS
+	catch( daw::json::json_exception const &jex ) {
+		std::cerr << "Exception thrown by parser: " << jex.reason( ) << '\n';
+		exit( 1 );
+	} catch( std::exception const &ex ) {
+		std::cerr << "Unknown exception thrown during testing: " << ex.what( )
+		          << '\n';
+		exit( 1 );
+	} catch( ... ) {
+		std::cerr << "Unknown exception thrown during testing\n";
+		throw;
+	}
+#endif
+} // namespace
+
+int main( int argc, char **argv )
+#ifdef DAW_USE_EXCEPTIONS
+  try
+#endif
+{
+	using namespace daw::json;
+	if( argc < 2 ) {
+		std::cerr << "Must supply a filenames to open\n";
+		exit( 1 );
+	}
+
+	std::string const json_data = [argv] {
+		auto const mmf = *daw::read_file( argv[1] );
+		test_assert( mmf.size( ) > 2, "Minimum json data size is 2 '{}'" );
+		return std::string( mmf.data( ), mmf.size( ) );
+	}( );
+
+	auto const sz = json_data.size( );
+	std::cout << "Processing: " << daw::utility::to_bytes_per_second( sz )
+	          << '\n';
+	test<constexpr_exec_tag>( json_data );
+	test<runtime_exec_tag>( json_data );
+	if constexpr( not std::is_same_v<runtime_exec_tag, simd_exec_tag> ) {
+		test<simd_exec_tag>( json_data );
+	}
+
+	// ******************************
+	// Test serialization
+	std::optional<daw::twitter::twitter_object_t> twitter_result =
+	  daw::json::from_json<daw::twitter::twitter_object_t>( json_data );
+	std::string str{ };
+	auto out_it = std::back_inserter( str );
+	daw::bench_n_test_mbs<DAW_NUM_RUNS>(
+	  "twitter bench(to_json_string)", sz,
+	  [&]( auto const &tr ) {
+		  str.clear( );
+		  daw::json::to_json( *tr, out_it );
+		  daw::do_not_optimize( str );
+	  },
+	  twitter_result );
+	test_assert( not str.empty( ), "Expected a string value" );
+	daw::do_not_optimize( str );
+	auto const twitter_result2 =
+	  daw::json::from_json<daw::twitter::twitter_object_t>( str );
+	daw::do_not_optimize( twitter_result2 );
+}
+#ifdef DAW_USE_EXCEPTIONS
+catch( daw::json::json_exception const &jex ) {
+	std::cerr << "Exception thrown by parser: " << jex.reason( ) << '\n';
+	exit( 1 );
+} catch( std::exception const &ex ) {
+	std::cerr << "Unknown exception thrown during testing: " << ex.what( )
+	          << '\n';
+	exit( 1 );
+} catch( ... ) {
+	std::cerr << "Unknown exception thrown during testing\n";
+	throw;
+}
+#endif
