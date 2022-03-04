a97b04f0b489580ec9a2729538320b8fc38287a9
beached <beached@users.noreply.github.com>
2022-03-04 23:14:16 +0000

    Deploying to gh-pages from @ beached/daw_json_link@226c54836c5957332bcb47badfd89131a5b13f5b 🚀

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cookbook_876',['CookBook',['../index.html',1,'']]],
-  ['classes_877',['Classes',['../md_cookbook_class.html',1,'']]],
-  ['classes_20from_20array_27s_878',['Classes from array&apos;s',['../md_cookbook_class_from_array.html',1,'']]]
+  ['classes_876',['Classes',['../md_cookbook_class.html',1,'']]],
+  ['classes_20from_20array_27s_877',['Classes from array&apos;s',['../md_cookbook_class_from_array.html',1,'']]],
+  ['cookbook_878',['CookBook',['../md_cookbook_readme.html',1,'']]]
 ];
