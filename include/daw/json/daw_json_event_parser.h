f980e1b33981c2d39de51a01ce15ed860a22a946
Darrell Wright <beached@users.noreply.github.com>
2022-05-26 00:56:54 -0400

    another fix for merge issue

--- include/daw/json/daw_json_event_parser.h
+++ include/daw/json/daw_json_event_parser.h
@@ -359,9 +359,10 @@ namespace daw::json {
 		template<typename ParseState = DefaultParsePolicy,
 		         typename StackContainerPolicy = use_default, typename Handler,
 		         auto... ParseFlags>
-		inline constexpr void
-		json_event_parser( basic_json_value<ParseState> bjv, Handler &&handler,
-		                   options::parse_flags_t<ParseFlags...> ) {
+		inline constexpr void json_event_parser(
+		  basic_json_value<ParseState> bjv, Handler &&handler,
+		  options::parse_flags_t<ParseFlags...> ) {
+
 			using ParsePolicy =
 			  typename ParseState::template SetPolicyOptions<ParseFlags...>;
 			auto jvalue = basic_json_value<ParsePolicy>( bjv );
@@ -447,9 +448,6 @@ namespace daw::json {
 					case json_parse_handler_result::Continue:
 						break;
 					}
-					parent_stack.push_back(
-					  { StackParseStateType::Class,
-					    std::pair<iterator, iterator>( jv.begin( ), jv.end( ) ) } );
 				} break;
 				case JsonBaseParseTypes::Bool: {
 					auto result = json_details::handle_on_bool( handler, jv );
@@ -504,105 +502,6 @@ namespace daw::json {
 						break;
 					}
 				} break;
-				case JsonBaseParseTypes::Bool: {
-					auto result = json_details::handle_on_bool( handler, jv );
-					switch( result.value ) {
-					case json_parse_handler_result::Complete:
-						parent_stack.clear( );
-						return;
-					case json_parse_handler_result::SkipClassArray:
-						move_to_last( );
-						return;
-					case json_parse_handler_result::Continue:
-						break;
-					}
-				} break;
-				case JsonBaseParseTypes::String: {
-					auto result = json_details::handle_on_string( handler, jv );
-					switch( result.value ) {
-					case json_parse_handler_result::Complete:
-						parent_stack.clear( );
-						return;
-					case json_parse_handler_result::SkipClassArray:
-						move_to_last( );
-						return;
-					case json_parse_handler_result::Continue:
-						break;
-					}
-				} break;
-				case JsonBaseParseTypes::Null: {
-					auto result = json_details::handle_on_null( handler, jv );
-					switch( result.value ) {
-					case json_parse_handler_result::Complete:
-						parent_stack.clear( );
-						return;
-					case json_parse_handler_result::SkipClassArray:
-						move_to_last( );
-						return;
-					case json_parse_handler_result::Continue:
-						break;
-					}
-				} break;
-				case JsonBaseParseTypes::None:
-				default: {
-					auto result = json_details::handle_on_error( handler, jv );
-					switch( result.value ) {
-					case json_parse_handler_result::Complete:
-						parent_stack.clear( );
-						return;
-					case json_parse_handler_result::SkipClassArray:
-						move_to_last( );
-						return;
-					case json_parse_handler_result::Continue:
-						break;
-					}
-				} break;
-				}
-			};
-
-			auto const process_range = [&]( stack_value_t v ) {
-				if( v.value.first != v.value.second ) {
-					auto jv = *v.value.first;
-					++v.value.first;
-					parent_stack.push_back( DAW_MOVE( v ) );
-					process_value( DAW_MOVE( jv ) );
-				} else {
-					switch( v.type ) {
-					case StackParseStateType::Class: {
-						daw_json_assert_weak(
-						  ( class_depth > 0 ) &
-						    ( v.value.first.get_raw_state( ).has_more( ) and
-						      v.value.first.get_raw_state( ).front( ) == '}' ),
-						  ErrorReason::InvalidEndOfValue );
-						--class_depth;
-						auto result = json_details::handle_on_class_end( handler );
-						switch( result.value ) {
-						case json_parse_handler_result::Complete:
-							parent_stack.clear( );
-							return;
-						case json_parse_handler_result::SkipClassArray:
-						case json_parse_handler_result::Continue:
-							break;
-						}
-					} break;
-					case StackParseStateType::Array: {
-						daw_json_assert_weak(
-						  ( array_depth > 0 ) &
-						    ( v.value.first.get_raw_state( ).has_more( ) and
-						      v.value.first.get_raw_state( ).front( ) == ']' ),
-						  ErrorReason::InvalidEndOfValue );
-						--array_depth;
-						auto result = json_details::handle_on_array_end( handler );
-						switch( result.value ) {
-						case json_parse_handler_result::Complete:
-							parent_stack.clear( );
-							return;
-						case json_parse_handler_result::SkipClassArray:
-						case json_parse_handler_result::Continue:
-							break;
-						}
-					} break;
-					}
 				}
 			};
 
@@ -665,11 +564,12 @@ namespace daw::json {
 
 		template<typename ParseState = DefaultParsePolicy,
 		         typename StackContainerPolicy = use_default, typename Handler>
-		inline constexpr void json_event_parser( basic_json_value<ParseState> bjv,
-		                                         Handler &&handler ) {
-			json_event_parser( DAW_MOVE( bjv ), DAW_FWD( handler ),
-			                   options::parse_flags<> );
+		inline constexpr void json_event_parser(
+		  basic_json_value<ParseState> bjv, Handler &&handler ) {
+			json_event_parser( DAW_MOVE( bjv ), DAW_FWD( handler ), options::parse_flags<> );
 		}
+
+
 
 		template<typename ParsePolicy = DefaultParsePolicy, typename Handler,
 		         auto... ParseFlags>
@@ -682,12 +582,11 @@ namespace daw::json {
 		}
 
 		template<typename ParsePolicy = DefaultParsePolicy, typename Handler>
-		inline void json_event_parser( daw::string_view json_document,
-		                               Handler &&handler ) {
+		inline void
+		json_event_parser( daw::string_view json_document, Handler &&handler ) {
 
-			return json_event_parser<ParsePolicy>(
-			  basic_json_value<ParsePolicy>( json_document ),
-			  DAW_FWD2( Handler, handler ), options::parse_flags<> );
+			return json_event_parser<ParsePolicy>( basic_json_value<ParsePolicy>( json_document ),
+			                                       DAW_FWD2( Handler, handler ), options::parse_flags<> );
 		}
 
 	} // namespace DAW_JSON_VER
