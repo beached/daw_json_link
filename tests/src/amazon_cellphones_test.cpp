d37631698b4e47e6fbe08294d77c6a244e2a95fb
Darrell Wright <beached@users.noreply.github.com>
2022-06-17 10:53:35 -0400

    adding more context to failure message

--- tests/src/amazon_cellphones_test.cpp
+++ tests/src/amazon_cellphones_test.cpp
@@ -131,6 +131,7 @@ int main( int argc, char **argv )
 #ifdef DAW_USE_EXCEPTIONS
 catch( daw::json::json_exception const &jex ) {
 	std::cerr << "Exception thrown by parser: " << jex.reason( ) << '\n';
+	std::cerr << to_formatted_string( jex ) << '\n';
 	exit( 1 );
 } catch( std::exception const &ex ) {
 	std::cerr << "Unknown exception thrown during testing: " << ex.what( )
