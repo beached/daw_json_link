188141838bf4038b5a89b8871a1a94b2780bf444
Darrell Wright <beached@users.noreply.github.com>
2023-08-01 21:59:56 -0400

    Allowing for noexcpet in more spots added clang++-17

--- include/daw/json/impl/daw_json_default_constuctor.h
+++ include/daw/json/impl/daw_json_default_constuctor.h
@@ -53,13 +53,17 @@ namespace daw::json {
 					++it;
 				} else {
 #endif
-#if not defined( DAW_JSON_DONT_USE_EXCEPTIONS )
-					if( std::uncaught_exceptions( ) == 0 ) {
-#endif
+					if constexpr( noexcept( ++it ) ) {
 						++it;
+					} else {
 #if not defined( DAW_JSON_DONT_USE_EXCEPTIONS )
-					}
+						if( std::uncaught_exceptions( ) == 0 ) {
 #endif
+							++it;
+#if not defined( DAW_JSON_DONT_USE_EXCEPTIONS )
+						}
+#endif
+					}
 #if defined( DAW_HAS_CONSTEXPR_SCOPE_GUARD )
 				}
 #endif
