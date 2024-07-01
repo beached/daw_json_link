2f4fbd22432e2873fb189b26eb588dafd0c0093d
Darrell Wright <beached@users.noreply.github.com>
2024-07-01 11:22:35 -0400

    pulled 428 from full test suite for now

--- tests/src/issue_428_test.cpp
+++ tests/src/issue_428_test.cpp
@@ -8,76 +8,82 @@
 
 #include <daw/json/daw_json_link.h>
 
-#include <fstream>
 #include <iostream>
 #include <type_traits>
 #include <vector>
 
-template <class Key, class T>
+template<class Key, class T>
 struct vector_map {
-    using value_type = std::pair<Key, T>;
-    using vector_type = std::vector<value_type>;
-    using reference = typename vector_type::reference;
-    using const_reference = typename vector_type::const_reference;
-    using iterator = typename vector_type::iterator;
-    using const_iterator = typename vector_type::const_iterator;
+	using value_type = std::pair<Key, T>;
+	using vector_type = std::vector<value_type>;
+	using reference = typename vector_type::reference;
+	using const_reference = typename vector_type::const_reference;
+	using iterator = typename vector_type::iterator;
+	using const_iterator = typename vector_type::const_iterator;
 
-   private:
-    vector_type data_;
+private:
+	vector_type data_;
 
-   public:
-    vector_map() = default;
+public:
+	vector_map( ) = default;
 
-    template <typename Container>
-        requires(!std::is_same_v<vector_map, std::remove_cvref_t<Container>>)
-    vector_map(Container &&values) {
-        for (auto &&v : values) {
-        }
-    }
+	template<typename Container,
+	         std::enable_if_t<
+	           not std::is_same_v<vector_map, daw::remove_cvref_t<Container>>,
+	           std::nullptr_t> = nullptr>
+	vector_map( Container &&values ) {
+		for( auto &&v : values ) {}
+	}
 
-    template <class... Args>
-    std::pair<iterator, bool> emplace(Args &&...args) {
-        data_.emplace_back(std::make_pair(std::forward<Args>(args)...));
-        return std::make_pair(data_.end() - 1, true);
-    }
+	template<class... Args>
+	std::pair<iterator, bool> emplace( Args &&...args ) {
+		data_.emplace_back( std::make_pair( std::forward<Args>( args )... ) );
+		return std::make_pair( data_.end( ) - 1, true );
+	}
 };
 
-template <typename class_type>
+template<typename class_type>
 struct map_builder {
-    constexpr class_type operator()() const { return class_type{}; }
-    constexpr class_type operator()(auto &&v) const { return class_type(v); }
+	constexpr class_type operator( )( ) const {
+		static_assert( std::is_same_v<int, class_type> );
+		return class_type{ };
+	}
 
-    template <typename Iter>
-    constexpr class_type operator()(Iter first, Iter last) const {
-        class_type ans;
-        while (first != last) {
-            auto [k, v] = *first;
-            ans.emplace(k, v);
-            ++first;
-        }
-        return ans;
-    }
+	template<typename T>
+	constexpr class_type operator( )( T &&v ) const {
+		static_assert( std::is_same_v<int, class_type> );
+		return class_type( v );
+	}
+
+	template<typename Iter>
+	constexpr class_type operator( )( Iter first, Iter last ) const {
+		static_assert( std::is_same_v<int, class_type> );
+		class_type ans;
+		while( first != last ) {
+			auto [k, v] = *first;
+			ans.emplace( k, v );
+			++first;
+		}
+		return ans;
+	}
 };
 
 namespace daw::json {
-template <class Key, class T>
-struct json_data_contract<vector_map<Key, T>> {
-    using class_type = vector_map<Key, T>;
+	template<class Key, class T>
+	struct json_data_contract<vector_map<Key, T>> {
+		using class_type = vector_map<Key, T>;
 
-    using constructor_t = map_builder<class_type>;
-    using type = json_type_alias<
-        json_key_value_no_name<class_type, Key, T, map_builder<class_type>>>;
-};
-}  // namespace daw::json
+		//using constructor_t = map_builder<class_type>;
+		using type = json_type_alias<
+		  json_key_value_no_name<class_type, Key, T, map_builder<class_type>>>;
+	};
+} // namespace daw::json
 
-void json_test(std::string_view data_view) {
-    auto obj = daw::json::from_json<vector_map<std::string, int>>(data_view);
-    auto obj_json = daw::json::to_json(
-        obj, daw::json::options::output_flags<
-                 daw::json::options::SerializationFormat::Pretty>);
+void json_test( std::string_view data_view ) {
+	auto obj = daw::json::from_json<vector_map<std::string, int>>( data_view );
+	auto obj_json = daw::json::to_json(
+	  obj, daw::json::options::output_flags<
+	         daw::json::options::SerializationFormat::Pretty> );
 
-    std::cout << obj_json.c_str() << std::endl;
+	std::cout << obj_json.c_str( ) << std::endl;
 }
-
-
-
