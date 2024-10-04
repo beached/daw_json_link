76697ed500618fae4829f6b7026cfc7853f88894
Darrell Wright <beached@users.noreply.github.com>
2024-10-04 13:19:12 -0400

    Refactor JSON reflection; add experimental namespace
    Refactored the `daw::json` namespace to include a nested `experimental` namespace, improving organization and readability of JSON reflection utilities. The change introduces slight adjustments to function naming and spacing, ensuring consistency and maintainability across the codebase.

--- include/daw/json/daw_json_reflection.h
+++ include/daw/json/daw_json_reflection.h
@@ -18,217 +18,230 @@
 #include <utility>
 #include <vector>
 
-/*
-namespace std::meta {
-  namespace __impl {
-    template<auto... vals>
-    struct replicator_type {
-      template<typename F>
-        constexpr void operator>>(F body) const {
-        (body.template operator()<vals>(), ...);
-      }
-    };
-
-    template<auto... vals>
-    replicator_type<vals...> replicator = {};
-  }
-}
-*/
-
-namespace daw::json {
-	inline namespace DAW_JSON_VER {
-	  struct rename_json {
-	    char const * name;
-	  };
-	  
-	  namespace refl_details {
-	    template<auto... vals>
-      struct replicator_type {
-        template<typename F>
-        constexpr void operator>>(F body) const {
-          (body.template operator()<vals>(), ...);
-        }
+namespace daw::json
+{
+  inline namespace
+  DAW_JSON_VER
+  {
+    inline namespace experimental
+    {
+      struct rename_json
+      {
+        char const* name;
       };
 
-	    template<auto... vals>
-      replicator_type<vals...> replicator = {};
-	    /// Get the public non-static data members
-	    consteval std::vector<std::meta::info>
-            pub_nsdm_of( std::meta::info type_class ) {
-	      auto members = std::meta::nonstatic_data_members_of( type_class );
-	      std::erase_if( members, []( std::meta::info const &i ) {
-                      return not std::meta::is_public( i );
-              } );
-	      return members;
-	    }
+      namespace refl_details
+      {
+        template <auto... vals>
+        struct replicator_type
+        {
+          template <typename F>
+          constexpr void operator>>(F body) const
+          {
+            (body.template operator()<vals>(), ...);
+          }
+        };
 
-	    template<typename R>
-      consteval auto expand(R range) {
-	      auto args = std::vector<std::meta::info>( );
-	      for (auto r : range) {
-	        args.push_back(std::meta::reflect_value(r));
-	      }
-	      return std::meta::substitute(^^replicator, args);
-	    }
+        template <auto... vals>
+        replicator_type<vals...> replicator = {};
+        /// Get the public non-static data members
+        consteval std::vector<std::meta::info>
+        pub_nsdm_of(std::meta::info type_class)
+        {
+          auto members = std::meta::nonstatic_data_members_of(type_class);
+          std::erase_if(members, [](std::meta::info const& i)
+          {
+            return not std::meta::is_public(i);
+          });
+          return members;
+        }
 
-      template <typename T>
-      consteval std::optional<T> get_annotaion( std::meta::info r ) {
-	      for( std::meta::info a: std::meta::annotations_of( r ) ) {
-	        if( std::meta::type_of( a ) == ^^T ) {
-	          return std::meta::extract<T>( a );
-	        }
-	      }
-	      return std::nullopt;
-	    }
+        template <typename R>
+        consteval auto expand(R range)
+        {
+          auto args = std::vector<std::meta::info>();
+          for (auto r : range)
+          {
+            args.push_back(std::meta::reflect_value(r));
+          }
+          return std::meta::substitute(^^replicator, args);
+        }
 
-	    template <typename T>
-      consteval bool has_annotation(std::meta::info r, T const& value) {
-	      auto expected = std::meta::reflect_value(value);
-	      for (std::meta::info a : std::meta::annotations_of(r)) {
-	        if (value_of(a) == expected) {
-	          return true;
-	        }
-	      }
-	      return false;
-	    }
+        template <typename T>
+        consteval std::optional<T> get_annotaion(std::meta::info r)
+        {
+          for (std::meta::info a : std::meta::annotations_of(r))
+          {
+            if (std::meta::type_of(a) == ^ ^ T)
+            {
+              return std::meta::extract<T>(a);
+            }
+          }
+          return std::nullopt;
+        }
 
-	    template<typename T, std::size_t... Is>
-            consteval auto to_tuple( T const &value, std::index_sequence<Is...> )
-              -> decltype( std::tuple( value.[:pub_nsdm_of( ^^T )[Is]:]... ) ) {
+        template <typename T>
+        consteval bool has_annotation(std::meta::info r, T const& value)
+        {
+          auto expected = std::meta::reflect_value(value);
+          for (std::meta::info a : std::meta::annotations_of(r))
+          {
+            if (value_of(a) == expected)
+            {
+              return true;
+            }
+          }
+          return false;
+        }
 
-	      return std::tuple( value.[:pub_nsdm_of( ^^T )[Is]:]... );
-	    }
+        template <typename T, std::size_t... Is>
+        consteval auto to_tuple(T const& value, std::index_sequence<Is...>)
+          -> decltype( std::tuple(value.[:pub_nsdm_of(^^T)[Is]:]...) )
+        {
+          return std::tuple(value.[:pub_nsdm_of(^^T)[Is]:]...);
+        }
 
-	    template<typename T>
-            consteval auto to_tuple( T const &value )
-              -> decltype( refl_details::to_tuple(
-                value, std::make_index_sequence<pub_nsdm_of( ^^T ).size( )>{ } ) ) {
-	      return refl_details::to_tuple(
-                value, std::make_index_sequence<pub_nsdm_of( ^^T ).size( )>{ } );
-	    }
+        template <typename T>
+        consteval auto to_tuple(T const& value)
+          -> decltype( refl_details::to_tuple(
+            value, std::make_index_sequence<pub_nsdm_of(^^T).size()>{}) )
+        {
+          return refl_details::to_tuple(
+            value, std::make_index_sequence<pub_nsdm_of(^^T).size()>{});
+        }
 
-	    template<JSONNAMETYPE Name, typename T>
-            using deduce_t = typename json_details::ensure_mapped_t<
-              json_details::json_deduced_type<T>>::template with_name<Name>;
+        template <JSONNAMETYPE Name, typename T>
+        using deduce_t = typename json_details::ensure_mapped_t<
+          json_details::json_deduced_type<T>>::template with_name<Name>;
 
-	    template<typename T, std::size_t Idx>
-            using submember_type_t =
-              std::tuple_element_t<Idx, DAW_TYPEOF( refl_details::to_tuple(
-                                          std::declval<T>( ) ) )>;
+        template <typename T, std::size_t Idx>
+        using submember_type_t =
+        std::tuple_element_t<Idx, DAW_TYPEOF(refl_details::to_tuple(
+                               std::declval<T>( ) ))>;
 
-	    template<typename T, std::size_t Idx>
-      consteval auto get_member_link_func( ) {
-	      constexpr auto name = [] {
-	        constexpr auto m = pub_nsdm_of( ^^T )[Idx];
-	        constexpr auto annot = get_annotaion<daw::json::rename_json>( m );
-	        if constexpr( annot ) {
-	          return std::string_view( annot->name ); 
-	        } else {
-	          return std::meta::identifier_of( m );
-	        }
-	      }( );
-	      static_assert( not name.empty( ) );
+        template <typename T, std::size_t Idx>
+        consteval auto get_member_link_func()
+        {
+          constexpr auto name = []
+          {
+            constexpr auto m = pub_nsdm_of(^^T)[Idx];
+            constexpr auto annot = get_annotaion<daw::json::rename_json>(m);
+            if constexpr (annot)
+            {
+              return std::string_view(annot->name);
+            }
+            else
+            {
+              return std::meta::identifier_of(m);
+            }
+          }();
+          static_assert(not name.empty());
 
-	      return daw::traits::identity<deduce_t<
-                json_name<name.size( ) + 1>(
-                  name.data( ), std::make_index_sequence<name.size( ) + 1>{ } ),
-                submember_type_t<T, Idx>>>{ };
-	    }
+          return daw::traits::identity<deduce_t<
+            json_name<name.size() + 1>(
+              name.data(), std::make_index_sequence<name.size() + 1>{}),
+            submember_type_t<T, Idx>>>{};
+        }
 
-	    template<typename T, std::size_t Idx>
-            using get_member_link_t =
-              typename DAW_TYPEOF( get_member_link_func<T, Idx>( ) )::type;
+        template <typename T, std::size_t Idx>
+        using get_member_link_t =
+        typename DAW_TYPEOF(get_member_link_func<T, Idx>( ))::type;
 
-	    template<typename, typename>
-            struct make_data_contract;
+        template <typename, typename>
+        struct make_data_contract;
 
-	    template<typename, typename...>
-            inline constexpr bool construction_test_v = false;
+        template <typename, typename...>
+        inline constexpr bool construction_test_v = false;
 
-	    template<typename T, typename... Ts>
-            inline constexpr bool construction_test_v<T, std::tuple<Ts...>> =
-              requires {
-	      T{ std::declval<Ts>( )... };
-              };
+        template <typename T, typename... Ts>
+        inline constexpr bool construction_test_v<T, std::tuple<Ts...>> =
+          requires
+          {
+            T{std::declval<Ts>()...};
+          };
 
-	    template<typename T>
-      concept Reflectable =
-              not std::is_empty_v<T> and std::is_class_v<T> and requires {
-	      refl_details::to_tuple( std::declval<T>( ) );
-              }
-	    and construction_test_v<T, DAW_TYPEOF( refl_details::to_tuple(
-                                         std::declval<T>( ) ) )>;
+        template <typename T>
+        concept Reflectable =
+          not std::is_empty_v<T> and std::is_class_v<T> and requires
+          {
+            refl_details::to_tuple(std::declval<T>());
+          }
+          and construction_test_v<T, DAW_TYPEOF(refl_details::to_tuple(
+                                    std::declval<T>( ) ))>;
 
-	    template<Reflectable T, std::size_t... Is>
-            struct make_data_contract<T, std::index_sequence<Is...>> {
-	      using type = json_member_list<get_member_link_t<T, Is>...>;
+        template <Reflectable T, std::size_t... Is>
+        struct make_data_contract<T, std::index_sequence<Is...>>
+        {
+          using type = json_member_list<get_member_link_t<T, Is>...>;
 
-	      DAW_ATTRIB_INLINE static constexpr auto to_json_data( T const &value ) {
-	        return daw::forward_nonrvalue_as_tuple(
-                  value.[:pub_nsdm_of( ^^T )[Is]:]... );
-	      }
-	    };
+          DAW_ATTRIB_INLINE static constexpr auto to_json_data(T const& value)
+          {
+            return daw::forward_nonrvalue_as_tuple(
+              value.[:pub_nsdm_of(^^T)[Is]:]...);
+          }
+        };
+      }
 
-	  }
+      inline constexpr auto reflectable = std::monostate{};
 
-	  inline constexpr auto reflectable = std::monostate{};
+      template <refl_details::Reflectable T>
+      inline constexpr bool is_reflectible_type_v = refl_details::has_annotation(^^T, reflectable);
 
-		template<refl_details::Reflectable T>
-		inline constexpr bool is_reflectible_type_v = refl_details::has_annotation( ^^T, reflectable );
-	 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_json_type; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_json_type; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_json_member_list; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_json_member_list; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_json_deduced_ordered_class; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_json_deduced_ordered_class; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_json_map_alias; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_json_map_alias; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_deduced_empty_class; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_deduced_empty_class; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_json_nullable; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_json_nullable; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_tagged_variant; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_tagged_variant; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_json_tuple_member_list; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_json_tuple_member_list; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_serialization_policy; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_serialization_policy; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_submember_tagged_variant; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_submember_tagged_variant; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_an_ordered_member; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_an_ordered_member; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
 
-		template<refl_details::Reflectable T>
-		requires( requires { typename T::i_am_a_parse_policy; } ) //
-		  inline constexpr bool is_reflectible_type_v<T> = false;
+      template <refl_details::Reflectable T>
+        requires(requires { typename T::i_am_a_parse_policy; }) //
+      inline constexpr bool is_reflectible_type_v<T> = false;
+    }
 
-		template<typename T>
-		requires is_reflectible_type_v<T> //
-		  struct json_data_contract<T>
-		  : refl_details::make_data_contract<
-		      T,
-		      std::make_index_sequence<refl_details::pub_nsdm_of( ^^T ).size( )>> {};
-	} // namespace DAW_JSON_VER
-} // namespace daw::json
+    template <typename T>
+      requires is_reflectible_type_v<T> //
+    struct json_data_contract<T>
+      : refl_details::make_data_contract<
+        T,
+        std::make_index_sequence<refl_details::pub_nsdm_of(^^T).size()>> {
+    };
+  }
+}
