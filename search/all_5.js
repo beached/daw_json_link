827a9e81a7de9007c455d3be8c276eec10c469c5
beached <beached@users.noreply.github.com>
2026-07-26 04:13:46 +0000

    Deploying to gh-pages from @ beached/daw_json_link@c69ac7c9f4b2c447e9058d7cf36c8ab1379898ee 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['70ghz_0',['Linux 5.3.0-29 32GB Intel(R) Core(TM) i7-7500U CPU @ 2.70GHz',['../md_docs_2kostya__benchmark__results.html#autotoc_md173',1,'']]],
-  ['7500u_20cpu_202_2070ghz_1',['Linux 5.3.0-29 32GB Intel(R) Core(TM) i7-7500U CPU @ 2.70GHz',['../md_docs_2kostya__benchmark__results.html#autotoc_md173',1,'']]]
+  ['70ghz_0',['Linux 5.3.0-29 32GB Intel(R) Core(TM) i7-7500U CPU @ 2.70GHz',['../md_docs_2kostya__benchmark__results.html#autotoc_md175',1,'']]],
+  ['7500u_20cpu_202_2070ghz_1',['Linux 5.3.0-29 32GB Intel(R) Core(TM) i7-7500U CPU @ 2.70GHz',['../md_docs_2kostya__benchmark__results.html#autotoc_md175',1,'']]]
 ];
