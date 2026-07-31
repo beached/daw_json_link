cc5f1171cfcbf443abf1f264c8014d32f5a14ed2
beached <beached@users.noreply.github.com>
2026-07-31 04:41:56 +0000

    Deploying to gh-pages from @ beached/daw_json_link@550d69d853292082d04f161f692c900338662917 🚀

--- search/enumvalues_8.js
+++ search/enumvalues_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['maybe_0',['Maybe',['../namespacedaw_1_1json_1_1anonymous__namespace_02daw__json__type__options_8h_03_1_1options.html#a193ae8db7d56d42595ecf98b37fea707a80ba31cc9d111c8ef880737a23ab0c54',1,'daw::json::anonymous_namespace{daw_json_type_options.h}::options']]],
-  ['minified_1',['Minified',['../namespacedaw_1_1json_1_1anonymous__namespace_02daw__json__serialize__options_8h_03_1_1options_1_1serialize__options.html#a0c4c307e2139348509b65444a9638bd6a79b186a99b0c69a5c68b3399e5b7ed10',1,'daw::json::anonymous_namespace{daw_json_serialize_options.h}::options::serialize_options']]]
+  ['minified_1',['Minified',['../namespacedaw_1_1json_1_1anonymous__namespace_02daw__json__serialize__options_8h_03_1_1options_1_1serialize__options.html#a0c4c307e2139348509b65444a9638bd6a79b186a99b0c69a5c68b3399e5b7ed10',1,'daw::json::anonymous_namespace{daw_json_serialize_options.h}::options::serialize_options']]],
+  ['minimum_2',['Minimum',['../namespacedaw_1_1json_1_1anonymous__namespace_02daw__json__type__options_8h_03_1_1options.html#a2daf504eec0026f70b87dd83b240be2caa1d0ec6d56f8833a078b5a7ac4caf2d4',1,'daw::json::anonymous_namespace{daw_json_type_options.h}::options']]]
 ];
